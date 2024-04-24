88adf13866e96810356c733282b3fcc0545dff6a
Kristina Davydova <tutsilianna@gmail.com>
2024-04-24 08:58:52 +0400

    project structure

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Greetings_and_Wishes_Generator
-AI Greetings &amp; Wishes Generator - educational group project as part of the course on neural networks (NLP) at ITMO 
+Greetings & Wishes Generator - educational group project as part of the course on neural networks (NLP) at ITMO 
+
+# Project structure 
+```
+├───data
+│   ├───raw                     
+│   │   └───greetings.csv                  # csv file with all greetings & wishes
+│   ├───processed
+│   │   ├───train.txt                      # dataset for training
+│   │   └───valid.txt                        # dataset for validation
+│   └───model                              # weights and parameters for the GPT model
+├───src                         
+│   └───scrape.py                          # scrapes greetings & wishes from the website
+├───.gitignore
+├───requirements.txt                       # all required dependencies
+├───greetings_text.txt                     # output text
+├───greetings_promt.txt                    # input text
+├───Greetings_and_Wishes_Generator.ipynb   # trains the model on the training dataset, contains class with the tuned model, example how the inference works
+└───README.md
+```
---
+++ src/parse.py
@@ -0,0 +1,118 @@
+import time
+import pandas as pd
+from playwright.sync_api import sync_playwright
+
+
+class GreetingParse():
+    
+    def __init__(self) -> None:
+        self.url   = "https://www.pozdravuha.ru/"
+        self.menus = ['.filters.menu-block.bg2.menu-left-main', '.filters.menu-block.bg3.menu-left-subrazd',
+                      '.filters.menu-block.bg3.menu-left-subrazd']
+        self.text  = ['.item.pozdravuha_ru_text']
+        self.pages = '.btn.btn-default.paginator_a' # also used in function next_page()
+
+
+    def parse_menu(self, i: int) -> dict[str]:
+        data = {} 
+        menu = self.page.query_selector(self.menus[i])
+        if menu:
+            list_menu = menu.query_selector_all('a')
+            for item in list_menu:
+                item_menu = item.evaluate("""
+                    el => ({
+                        category: el.innerText,
+                        link: el.href
+                    })
+                """)
+                data[item_menu['category']] = item_menu['link']
+        return data
+
+    def count_pages(self) -> None:
+        items = self.page.query_selector_all(self.pages)
+       
+        if items:
+            return len(items) // 2 
+        else:
+            return 1
+
+    def next_page(self) -> None:
+        self.page.evaluate("""
+            const elements = document.querySelectorAll('.btn.btn-default.paginator_a');
+            const lastElement = elements[elements.length - 1];
+
+            if (lastElement) {
+                lastElement.click();
+            }
+        """)
+        time.sleep(4)
+
+
+    def parse_text(self, url: str, holiday: str, to: str, description: str) -> None:
+        data = {'text': [], 'likes': [], 'holiday': [], 'to': [], 'description': []}
+
+        self.page.goto(url=url, timeout=60000)
+        time.sleep(2)
+        '''проверяем количество страниц'''
+        count_pages = self.count_pages()
+        for _ in range(count_pages - 1):
+            items = self.page.query_selector_all(self.text[0])
+            for item in items:
+                item = item.evaluate("""
+                    el => {
+                        let text = el.innerText.replace(/<br>/g, '\\n');
+                        try{
+                            text = text.replace(/© Принадлежит сайту\. Автор: .*?\\n/g, '');
+                        } catch{}
+                        text = text.replace(/Создать открытку *?/g, '');
+                        // text = text.replace(/\d+/gm, '');
+                        let likes = el.querySelector('.badge') ? el.querySelector('.badge').innerText : '';
+                        return {
+                            text: text.trim(),
+                            likes: likes
+                        };
+                    }
+                """)
+                data['text'].append(item['text'])
+                data['likes'].append(item['likes'])
+                data['to'].append(to)
+                data['description'].append(description)
+                data['holiday'].append(holiday)
+            df = pd.DataFrame(data)
+            df.to_csv('data/greetings.csv', index=False, encoding='utf-8', mode='a+', header=False)
+            self.next_page()
+
+    def get_data(self) -> None:
+        data_dict = self.parse_menu(i=0)
+        for x in data_dict:
+            self.page.goto(data_dict[x], timeout=60000)
+            time.sleep(2)
+            data_dict[x] = self.parse_menu(i=1)
+            if isinstance(data_dict[x], dict):
+                for y in data_dict[x]:
+                    self.page.goto(data_dict[x][y], timeout=60000)
+                    time.sleep(2)
+                    data_dict[x][y] = self.parse_menu(i=2)
+                    if data_dict[x][y]:
+                        for z in data_dict[x][y]:
+                            print('z: ' + z)
+                            self.parse_text(url=data_dict[x][y][z], holiday=x, to=y, description=z) # здесь собираем данные
+                            time.sleep(2)
+                            print(f'{x}   {y}   {z} [+]')
+                    else:
+                        self.parse_text(url=self.page.url, holiday=x, to=y, description='') # здесь собираем данные
+                        time.sleep(2)
+                        print(f'{x}   {y} [+]')
+
+    def parse(self) -> None:
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            self.context = browser.new_context()
+            self.page = self.context.new_page()
+            self.page.goto(self.url)
+            # self.page.wait_for_load_state("load") 
+            time.sleep(5)
+            self.get_data_()
+
+if __name__ == "__main__":
+    GreetingParse().parse()
